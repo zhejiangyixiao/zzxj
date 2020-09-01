78423398960071a071bcf72aa3ed488d00fc74eb
yixiao <812244160@qq.com>
2020-09-01 20:31:16 +0800

    cart修改

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,15 +1,27 @@
 <template>
-    <div>
-        购物车
-    </div>
+  <div class="cart">
+    <van-nav-bar title="购物车"  :border="false" fixed placeholder>
+        <template #right>
+            <span class="nav_right">编辑</span>
+        </template>
+    </van-nav-bar>
+
+    <section class="content">
+
+    </section>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
-<style lang="stylus" scoped>
-
+<style lang="stylus">
+.van-nav-bar__title
+        font-weight 800
+.cart
+    background #f6f6f6
+    .nav_right
+        font-size 12px
+        color #aaa
 </style>
