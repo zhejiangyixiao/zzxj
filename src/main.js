7f72d6593d8d1384b5b5448cf111ecdc82ba1cba
whisper-Z <zena970210@163.com>
2020-09-02 22:07:59 +0800

    Homee基本完成

--- src/main.js
+++ src/main.js
@@ -3,32 +3,21 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 // 引入reset 去除自带的样式，变成怪异盒模型
-import "./assets/style/reset.stylus"
+import "./assets/style/reset.stylus";
 // 新的rem适配
-import "amfe-flexible";
+import "amfe-flexible/index.js";
 Vue.config.productionTip = false;
 
-
-import {
-    Tabbar,
-    TabbarItem,
-    NavBar,
-    Icon,
-    Swipe, 
-    SwipeItem,
-} from "vant";
-
+import { Tabbar, TabbarItem, Swipe, SwipeItem, Grid, GridItem } from "vant";
 Vue.use(Tabbar);
 Vue.use(TabbarItem);
-Vue.use(NavBar);
-Vue.use(Icon);
 Vue.use(Swipe);
 Vue.use(SwipeItem);
-
-
+Vue.use(Grid);
+Vue.use(GridItem);
 
 new Vue({
-    router,
-    store,
-    render: h => h(App)
+  router,
+  store,
+  render: h => h(App)
 }).$mount("#app");
