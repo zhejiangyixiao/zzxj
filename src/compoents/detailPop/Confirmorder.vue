a0e3a8e4e6b96b7fc3289256305ef8e5481e3e41
hu-1234 <185773179@qq.com>
2020-09-04 16:04:47 +0800

    上传1

--- src/compoents/detailPop/Confirmorder.vue
+++ src/compoents/detailPop/Confirmorder.vue
@@ -12,26 +12,58 @@
       <!-- 添加地址 -->
       <div class="dzl">
           <p>您需要我们送到哪里？</p>
-          <div class="dz van-hairline--top-bottom"></div>
-
+          <div class="dz" @click="toAddress">
+              <span>添加收获地址</span>
+              <van-icon name="arrow" :size="16" color="#8F8F8F"/>
+          </div>
+          <div class="xy">
+            <input type="checkbox">
+            <span>我已阅读并同意</span>
+            <a href="#" >《造作物流及售后协议》</a>
+          </div>
       </div>
       <!-- 价格明细 -->
       <div class="price-detail">
-
+          <p>您的订单价格明细</p>
+          <div class="price">
+              <li>
+                <span>商品合计</span>
+                <span class="jg">￥4998</span>
+                </li>
+              <li>
+                <span>运费</span>
+                <span class="jg">请选择地址</span>
+                </li>
+              <li>
+                <span>实付</span>
+                <span class="jg">￥4998</span>
+              </li>
+          </div>
       </div>
       <!-- 是否开发票 -->
       <div class="invoice">
-
+        <p>您是否需要发票？</p>
+        <div class="fp van-hairline--top">
+          <div class="one">
+             <input type="checkbox">
+             <span>我要开发票</span>
+          </div>
+          <van-icon name="arrow" color="#8F8F8F" :size="14" />
+        </div>
       </div>
       <!-- 备注添加 -->
       <div class="increase-remarks">
-
+        <p>如果还有别的需要，备注一句</p>
+       <textarea ></textarea>
       </div>
       <!-- 商品清单 -->
       <div class="qdList">
         <p>您的商品清单</p>
         <div class="goodList">
-
+            <div class="tophead">
+              <span>单品</span>
+            </div>
+            <li></li>
         </div>
       </div>
       <!-- 底部结算 -->
@@ -44,10 +76,18 @@
 </template>
 <script>
 export default {
+  data() {
+    return {
+      isColor:false
+    }
+  },
   methods: {
     onClickLeft(){
       this.$router.go(-1)
-    }
+    },
+    toAddress(){
+      this.$router.push('/address')
+    },
   },
 }
 </script>
@@ -71,34 +111,161 @@ export default {
       font-size 20px
       color #232323
       font-weight bold
-      .dz
-        width 315px
-        height 34px
-        background pink
+    .dz 
+      width 327px
+      height 34px
+      background #fff
+      margin-top 33px
+      margin-left 4px
+      display flex
+      justify-content space-between
+      color #8F8F8F
+      border-bottom 1px solid #8F8F8F
+      span 
+        font-size 13px
+    .xy
+      margin-top 22px
+      font-size 13px
+      height 16px
+      display flex
+      justify-content flex-start
+      align-items center
+      input 
+        width 16px
+        height 16px
+      input[type="checkbox"] 
+        -webkit-appearance none /*清除复选框默认样式*/
+        border 1px solid #000
+        vertical-align middle
+        width 18px
+        height 18px
+        float left
+        margin-top 0
+        margin-right 0 !important
+        border-radius 0
+      input[type="checkbox"]:checked 
+        background url(../../assets/im/icon-3.png) /*复选框的背景图*/
+        background-size 16px
+      span 
+        margin-left 11px
+        color #000
+      a
+        color #418D59
+        margin-left 18px
+      
   .price-detail
     margin-top 10px
     background #fff
     width 100%
     height 168px
+    padding-top 33px
+    padding-left 16px
+    p
+      font-size 20px
+      color #232323
+      font-weight bold
+    .price
+      width 311px
+      margin-left 4px
+      display  flex
+      flex-direction column
+      margin-top 20px
+      li
+        display flex
+        width 100%
+        justify-content space-between
+        font-size 13px
+        margin-bottom 10px
+        .jg
+          color #8F8F8F
   .invoice
     width 100%
     height 130px
     margin-top 10px
     background #fff
+    padding-top 20px
+    padding-left 16px
+    p
+      font-size 20px
+      color #232323
+      font-weight bold
+    .fp
+      width 327px
+      height 36px
+      background #fff
+      margin-top 19px
+      margin-left 4px
+      display flex
+      justify-content space-between
+      align-items flex-end
+      color #8F8F8F
+      .one
+        display flex
+        justify-content center
+        align-items center
+        color #000
+        font-size 12px
+        input 
+          width 16px
+          height 16px
+        input[type="checkbox"] 
+          -webkit-appearance none /*清除复选框默认样式*/
+          border 1px solid #000
+          vertical-align middle
+          width 18px
+          height 18px
+          float left
+          margin-top 0
+          margin-right 0 !important
+          border-radius 0
+        input[type="checkbox"]:checked 
+          background url(../../assets/im/icon-3.png) /*复选框的背景图*/
+          background-size 16px
+        span 
+          margin-left 12px 
+
   .increase-remarks
     width 100%
     height 183px
     margin-top 10px
     background #fff
+    padding-top 23px
+    padding-left 17px
+    p
+      font-size 20px
+      color #232323
+      font-weight bold
+    textarea
+      width 325px
+      height 76px
+      margin-left 4px
+      margin-top 23px
+      border 1px solid #8F8F8F
+
   .qdList
     width 353px
     margin-top 33px
     margin-left 11px
+    p
+      font-size 20px
+      color #232323
     .goodList
       margin-top 20px
       width 100%
-      height 100px
-      background pink
+      background #fff
+      .tophead 
+        width 100%
+        height 43px
+        background #fff
+        font-size 17px
+        color #232323
+        padding-top 16px
+      li 
+        width 100%
+        height 121px
+        border-top 1px solid #909090
+        background #fff
+        
   .js
     width 100%
     margin-top 18px
